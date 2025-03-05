01c799bd6815159be37e07eaa5f5de64a3365e49
tgnelson19 <nelson.tyler.g@gmail.com>
2025-03-04 21:34:44 -0600

    Weird bug with main code that cause the main function to not run, fixed now

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ while not vars.done:  # Loop to render each frame
 
     if vars.state == "titleScreen":
         vars.doTheTitleScreen()
+
+    elif vars.state == "gameRun":
         vars.doAnUpdate()  # Main function that collects event data, and paints the current frame to the screen
 
     elif vars.state == "leveling":
